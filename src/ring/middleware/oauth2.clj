b0c3f83ff0bbc6c06cc1a4d5f79933e250b81166
Lukas Studer <lukas.studer@distran.ch>
2025-01-15 20:12:50 +0100

    slightly refactor `refresh-one-token` for clarity

--- src/ring/middleware/oauth2.clj
+++ src/ring/middleware/oauth2.clj
@@ -210,20 +210,18 @@
 (def refresh-socket-timeout 60000)
 
 (defn- refresh-token-request-options [profile refresh-token]
-  (token-http-options profile {:grant_type "refresh_token"
-                               :refresh_token refresh-token}))
+  (-> (token-http-options profile {:grant_type "refresh_token"
+                                   :refresh_token refresh-token})
+      (assoc :socket-timeout refresh-socket-timeout)))
 
 (defn- refresh-one-token
   ([profile refresh-token]
-   (-> (refresh-token-request-options profile refresh-token)
-       (assoc :socket-timeout refresh-socket-timeout)
-       http/request
+   (-> (http/request (refresh-token-request-options profile refresh-token))
        format-access-token))
   ([profile refresh-token respond raise]
-   (-> (refresh-token-request-options profile refresh-token)
-       (assoc :async? true
-              :socket-timeout refresh-socket-timeout)
-       (http/request (comp respond format-access-token) raise))))
+   (let [opts (-> (refresh-token-request-options profile refresh-token)
+                  (assoc :async? true))]
+     (http/request opts (comp respond format-access-token) raise))))
 
 (defn- refresh-tasks [profiles access-tokens]
   (->> (expired-access-tokens access-tokens)
